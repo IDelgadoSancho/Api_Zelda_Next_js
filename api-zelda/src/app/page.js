4598a7dc01b17b290abb442a4ae6bd027dc88bce
Ismael Delgado Sancho <ismael_delgadosancho@iescarlesvallbona.cat>
2025-03-30 19:22:54 +0200

    add fade-in animation for logo and update home page layout with new images

--- api-zelda/src/app/page.js
+++ api-zelda/src/app/page.js
@@ -1,7 +1,21 @@
 export default function Page() {
   return (
     <>
-    <h1>home page</h1>
+      <div className="flex justify-start w-full z-10 relative pt-2 pl-40">
+        <img 
+          src="Zelda_flat.svg" 
+          alt="Zelda Logo" 
+          className="max-w-full h-auto fade-in-logo"
+        />
+      </div>
+      
+      <div className="w-full h-full absolute top-0 left-0 right-0 bottom-0 overflow-hidden -z-10">
+        <img
+          src="/zeldaIntro.gif"
+          className="w-full h-full object-cover"
+          alt="Intro Zelda"
+        />
+      </div>
     </>
   )
 }
